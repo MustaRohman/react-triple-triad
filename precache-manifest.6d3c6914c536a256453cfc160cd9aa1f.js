8ca3f84ec43a840febaf9fcd092a84b1bd95effd
Musta Rohman <mustarohman@Mustas-MBP.home>
2019-02-07 19:55:34 +0000

    Updates

---
+++ precache-manifest.6d3c6914c536a256453cfc160cd9aa1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e153e244061d441eecce",
+    "url": "/react-triple-triad/static/css/main.133c745e.chunk.css"
+  },
+  {
+    "revision": "e153e244061d441eecce",
+    "url": "/react-triple-triad/static/js/main.e153e244.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/react-triple-triad/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "2b538d2fd129467fa7a8",
+    "url": "/react-triple-triad/static/js/runtime~main.2b538d2f.js"
+  },
+  {
+    "revision": "27cba4069a5b7119d6a8f5e5a529af58",
+    "url": "/react-triple-triad/index.html"
+  }
+];
